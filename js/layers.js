d1d2f96e87215c1546b3dfc1539df3699e645384
lockstepgithub <mjeusebio09@gmail.com>
2022-07-30 01:53:35 +0800

    hello bricks

--- js/layers.js
+++ js/layers.js
@@ -24,5 +24,70 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            title: "le double",
+            description: "Double your point gain.",
+            cost: new Decimal(1),
+        },
+        12: {
+            title: "le scale",
+            description: "COST SCALING TIME!",
+            cost: new Decimal(2),
+            effect() {
+                return player[this.layer].points.add(1).pow(0.5)
+            },
+            effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+        },
+        13: {
+            title: "le scale 2?",
+            description: "COST SCALING TIME 2 ",
+            cost: new Decimal(5),
+            effect() {
+                return player.points.add(1).pow(0.15)
+            },
+            gainMult() {
+                let mult = new Decimal(1)
+                if (hasUpgrade('p', 13)) mult = mult.times(upgradeEffect('p', 13))
+                return mult
+            },
+        },
+    },
+})
+addLayer("b", {
+    name: "bricks", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#FF0000",
+    requires: new Decimal(1000), // Can be a function that takes requirement increases into account
+    resource: "bricks", // Name of prestige currency
+    baseResource: "points", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "b", description: "B: Reset for bricks", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true},
+    branches: ["p"],
+    upgrades: {
+        11: {
+            title: "le double",
+            description: "Double your presiege point gain.",
+            cost: new Decimal(1),
+        },
+    },
 })
